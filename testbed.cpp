e95f1713ffc84ef081f3984e64368010f13ebbb0
Zhuo Zhang <imzhuo@foxmail.com>
2021-11-11 23:43:07 +0800

    test: add the second test case

--- testbed.cpp
+++ testbed.cpp
@@ -16,6 +16,11 @@ TEST(ArabicToRomanNumeralsConverter, 1_isConvertedTo_I)
     ASSERT_EQ("I", convertArabicNumberToRomanNumeral(1));
 }
 
+TEST(ArabicToRomanNumeralsConverter, 2_isConvertedTo_II)
+{
+    ASSERT_EQ("II", convertArabicNumberToRomanNumeral(2));
+}
+
 int main(int argc, char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
