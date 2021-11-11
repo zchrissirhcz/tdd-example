62a0557fe06ce6be5fca1ab6fe4ed9e46458d2f4
Zhuo Zhang <imzhuo@foxmail.com>
2021-11-11 23:41:00 +0800

    feat: add the 1st test case and the fake impl

--- testbed.cpp
+++ testbed.cpp
@@ -1,8 +1,19 @@
 #include "gtest/gtest.h"
+#include <string>
+
+std::string convertArabicNumberToRomanNumeral(const unsigned int arabicNumber)
+{
+    return "";
+}
 
 TEST(ArabicToRomanNumeralsConverter, PreparationsCompleted)
 {
     GTEST_SUCCEED();
+}
+
+TEST(ArabicToRomanNumeralsConverter, 1_isConvertedTo_I)
+{
+    ASSERT_EQ("I", convertArabicNumberToRomanNumeral(1));
 }
 
 int main(int argc, char** argv)
