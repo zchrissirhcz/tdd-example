f169132206b933a12ef6fbcd890ea9d061014efe
Zhuo Zhang <imzhuo@foxmail.com>
2021-11-12 20:30:06 +0800

    feat: add more tests, fix convert 40

--- testbed.cpp
+++ testbed.cpp
@@ -8,13 +8,14 @@ struct ArabicToRomanMapping
     std::string romanNumeral;
 };
 
-const std::size_t numberOfMappings = 12;
+const std::size_t numberOfMappings = 13;
 using ArabicToRomanMappings = std::array<ArabicToRomanMapping, numberOfMappings>;
 
 const ArabicToRomanMappings arabicToRomanMappings = { {
     {1000, "M"},
     {900, "CM"},
     {500, "D"},
+    {400, "CD"},
     {100, "C"},
     {90, "XC"},
     {50, "L"},
@@ -63,24 +64,31 @@ TEST(ArabicToRomanNumeralsConverter, many_cases)
     assertThat(1).isConvertedToRomanNumeral("I");
     assertThat(2).isConvertedToRomanNumeral("II");
     assertThat(3).isConvertedToRomanNumeral("III");
+    assertThat(4).isConvertedToRomanNumeral("IV");
+    assertThat(5).isConvertedToRomanNumeral("V");
+    assertThat(6).isConvertedToRomanNumeral("VI");
+    assertThat(9).isConvertedToRomanNumeral("IX");
     assertThat(10).isConvertedToRomanNumeral("X");
     assertThat(20).isConvertedToRomanNumeral("XX");
     assertThat(30).isConvertedToRomanNumeral("XXX");
     assertThat(33).isConvertedToRomanNumeral("XXXIII");
+    assertThat(37).isConvertedToRomanNumeral("XXXVII");
+    assertThat(40).isConvertedToRomanNumeral("XL");
+    assertThat(50).isConvertedToRomanNumeral("L");
+    assertThat(90).isConvertedToRomanNumeral("XC");
+    assertThat(99).isConvertedToRomanNumeral("XCIX");
     assertThat(100).isConvertedToRomanNumeral("C");
     assertThat(200).isConvertedToRomanNumeral("CC");
     assertThat(300).isConvertedToRomanNumeral("CCC");
-    assertThat(1000).isConvertedToRomanNumeral("M");
-    assertThat(5).isConvertedToRomanNumeral("V");
-    assertThat(6).isConvertedToRomanNumeral("VI");
-    assertThat(37).isConvertedToRomanNumeral("XXXVII");
-    assertThat(50).isConvertedToRomanNumeral("L");
+    assertThat(499).isConvertedToRomanNumeral("CDXCIX");
     assertThat(500).isConvertedToRomanNumeral("D");
-    assertThat(4).isConvertedToRomanNumeral("IV");
-    assertThat(9).isConvertedToRomanNumeral("IX");
-    assertThat(40).isConvertedToRomanNumeral("XL");
-    assertThat(90).isConvertedToRomanNumeral("XC");
     assertThat(900).isConvertedToRomanNumeral("CM");
+    assertThat(1000).isConvertedToRomanNumeral("M");
+    assertThat(2000).isConvertedToRomanNumeral("MM");
+    assertThat(2017).isConvertedToRomanNumeral("MMXVII");
+    assertThat(3000).isConvertedToRomanNumeral("MMM");
+    assertThat(3333).isConvertedToRomanNumeral("MMMCCCXXXIII");
+    assertThat(3999).isConvertedToRomanNumeral("MMMCMXCIX");
 }
 
 int main(int argc, char** argv)
