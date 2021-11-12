8b3ded7da4c67642251a0c56cdb051a88fd3f8d1
Zhuo Zhang <imzhuo@foxmail.com>
2021-11-12 20:40:11 +0800

    refactor: split code into lib and test, adjust cmake

--- testbed.cpp
+++ testbed.cpp
@@ -1,45 +1,6 @@
 #include "gtest/gtest.h"
 #include <string>
-#include <array>
-
-struct ArabicToRomanMapping
-{
-    unsigned int arabicNumber;
-    std::string romanNumeral;
-};
-
-const std::size_t numberOfMappings = 13;
-using ArabicToRomanMappings = std::array<ArabicToRomanMapping, numberOfMappings>;
-
-const ArabicToRomanMappings arabicToRomanMappings = { {
-    {1000, "M"},
-    {900, "CM"},
-    {500, "D"},
-    {400, "CD"},
-    {100, "C"},
-    {90, "XC"},
-    {50, "L"},
-    {40, "XL"},
-    {10, "X"},
-    {9, "IX"},
-    {5, "V"},
-    {4, "IV"},
-    {1, "I"}
-} };
-
-std::string convertArabicNumberToRomanNumeral(unsigned int arabicNumber)
-{
-    std::string romanNumeral;
-    for (const auto& mapping: arabicToRomanMappings)
-    {
-        while (arabicNumber >= mapping.arabicNumber)
-        {
-            romanNumeral += mapping.romanNumeral;
-            arabicNumber -= mapping.arabicNumber;
-        }
-    }
-    return romanNumeral;
-}
+#include "ArabicToRomanNumeralConverter.hpp"
 
 class RomanNumeralAssert {
 public:
